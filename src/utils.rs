430334bbfd243c8df4ac07140627dc62e47ed763
igorbrasileiro <brasileiro456@gmail.com>
2022-05-16 15:33:40 -0300

    Add tests for utils module

--- src/utils.rs
+++ src/utils.rs
@@ -22,7 +22,7 @@ pub fn check_file_availability(filename: &str) -> String {
 
     let mut index = 1;
     let mut new_filename = format!(
-        "{filename} ({ind}).{ext}",
+        "./{filename} ({ind}).{ext}",
         filename = filename_without_extension,
         ind = index,
         ext = file_extension,
@@ -31,7 +31,7 @@ pub fn check_file_availability(filename: &str) -> String {
     while Path::new(&*new_filename).exists() {
         index += 1;
         new_filename = format!(
-            "{filename} ({ind}).{ext}",
+            "./{filename} ({ind}).{ext}",
             filename = filename_without_extension,
             ind = index,
             ext = file_extension,
@@ -45,6 +45,28 @@ pub fn check_file_availability(filename: &str) -> String {
 mod utils_tests {
     #[test]
     fn check_file_availability() {
-        assert_eq!("test.txt", super::check_file_availability("test.txt"));
+        // remove previous files
+        let _x = std::fs::remove_file("./test.txt");
+        let _x = std::fs::remove_file("./test (1).txt");
+
+        assert_eq!("./test.txt", super::check_file_availability("./test.txt"));
+
+        // Create test.txt file
+        let _x = std::fs::File::create("./test.txt");
+        assert_eq!(
+            "./test (1).txt",
+            super::check_file_availability("./test.txt")
+        );
+
+        // Create test (1).txt file
+        let _x = std::fs::File::create("./test (1).txt");
+        assert_eq!(
+            "./test (2).txt",
+            super::check_file_availability("./test.txt")
+        );
+
+        // remove created files during tests
+        let _x = std::fs::remove_file("./test.txt");
+        let _x = std::fs::remove_file("./test (1).txt");
     }
 }
