c14272442b3c82ba6cba2fbd12823e36aec44f76
igorbrasileiro <brasileiro456@gmail.com>
2023-02-02 10:31:16 -0300

    Improve release script

--- release.sh
+++ release.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 
+echo "Install cargo-bump. Run `cargo install cargo-bump`"
+
 cargo bump $1 && git add ./Cargo.toml && git commit -m "Bumping package version" && cargo publish && git push origin main -f
